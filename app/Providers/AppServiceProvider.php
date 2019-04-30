727d21d1e7a6eeb082162001196d267f114ac961
Zoltan Flamis <flamisz@gmail.com>
2019-04-30 22:27:37 +1200

    fix typo

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (config('app.env') === 'production') {
-            URL::forceSchema('https');
+            URL::forceScheme('https');
         }
     }
 }
